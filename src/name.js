682b6c9102ed4dce199573c48c4b5da5335cb610
Jun <wnsgur4092@gmail.com>
2024-03-30 02:27:20 +1100

    Update localStroage comment

--- src/name.js
+++ src/name.js
@@ -1,15 +1,16 @@
 //name.js
-
 export function setUpGreetingForm() {
   const form = document.querySelector('.greeting__form');
   const input = document.querySelector('.greeting__input');
   const greetingMessage = document.querySelector('.greeting__message');
 
+  //Local Storage에 name 불러오기
   const savedName = window.localStorage.getItem('name');
   if (savedName) {
     displayGreeting(savedName);
   }
 
+  //Local Storage에 name 저장
   form.addEventListener('submit', (event) => {
     event.preventDefault();
     const name = input.value.trim();
