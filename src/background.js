1fd795955e5ed80e2768b6ba92bc5b805fc27a95
Jun <wnsgur4092@gmail.com>
2024-04-10 21:15:08 +1000

    Refactoring

--- src/background.js
+++ src/background.js
@@ -1,5 +1,7 @@
 //background.js
 
+import { getRandomInt } from './util.js';
+
 export function changeBackground() {
   const maxImageCount = 8;
   const imageNumber = Math.floor(Math.random() * maxImageCount) + 1;
@@ -8,4 +10,10 @@ export function changeBackground() {
   document.body.style.backgroundImage = `url('${imageURL}')`;
   document.body.style.backgroundSize = 'cover';
   document.body.style.backgroundPosition = 'center';
+}
+
+const imageNumber = 8;
+
+export function paintBg() {
+  const imageNumber = getRandomInt(imageNumber);
 }
