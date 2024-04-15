3d7a35a88714e576a8b139574997b91dfbed380d
Jun <wnsgur4092@gmail.com>
2024-04-15 22:54:49 +1000

    Add Object.freeze in localStorage.js

--- src/constant/localStorage.js
+++ src/constant/localStorage.js
@@ -1,5 +1,7 @@
-export const LS = {
+export const LS = Object.freeze({
   COORDS: '@momentum-coords',
   CURRENT_USER: '@momentum-currentUser',
   TODOS: '@momentum-todos',
-};
+});
+
+//Object.freeze -> 외부에서 값을 변경할 수 없게 만든다
